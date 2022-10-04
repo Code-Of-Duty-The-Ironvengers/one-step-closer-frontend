dfbbce7f8f2584fc0f62c5200ccd429d0b9faa12
André de Albuquerque <itstheandre@gmail.com>
2022-10-04 19:36:55 +0200

    feat: add signup

--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    name: "",
+    password: "",
+  });
+  console.log("form:", form);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("Tried submitting the form");
+
+    axios
+      .post("http://localhost:5005/auth/signup", form)
+      .then((user) => {
+        console.log("user:", user);
+      })
+      .catch((err) => {
+        console.log("err:", err);
+      });
+  }
+
+  function handleChange(e) {
+    // console.log("input being worked on -> ", e.target.name);
+    // console.log("value added to input -> ", e.target.value);
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value });
+  }
+
+  const handleUsernameChange = (e) => setUsername(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+  const handleNameChange = (e) => setName(e.target.value);
+
+  return (
+    <div>
+      SignUp
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username
+          <input
+            onChange={handleChange}
+            type="text"
+            name="username"
+            value={form.username}
+          />
+        </label>
+        <br />
+        <label>
+          Name
+          <input
+            onChange={handleChange}
+            type="text"
+            name="name"
+            value={form.name}
+          />
+        </label>
+        <br />
+        <label>
+          Email
+          <input
+            onChange={handleChange}
+            type="text"
+            name="email"
+            value={form.email}
+          />
+        </label>
+        <br />
+        <label>
+          Password
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            value={form.password}
+          />
+        </label>
+        <br />
+
+        <button type="submit">Create your account</button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
